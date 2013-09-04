e330eae14f25b964ea7214690836a101e5ba0465
mackmoe <maurice.nash1@gmail.com>
2013-09-04 16:22:44 -0500

    Update connector.php
    added instructions on "how-to"

--- connector.php
+++ connector.php
@@ -8,7 +8,7 @@ Description: This script was written to test simple issues with a website's abil
 from the server(s) it lives on. Please feel free to use this script in any way you'd like, you don't have to ask! You may also re-use this in 
 anyway you'd like however, please give credit where credit is due. 
 Special thanks to Gene and Kenny, if it wern't for them, this script would never have been made!
-How to use script: Just upload the script to any web facing direcotry (content should do)
+How to use script: Just upload the script to any web facing direcotry (content should do) then in a browser, go to "domain.com/cs-connector/connector.php
 Notes: Use a captcha for mail and also use mysqli::real_escape_string for querries before they are sent
 -->
 <!DOCTYPE html>
